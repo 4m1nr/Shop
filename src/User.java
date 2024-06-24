eca85d2ece715fc3cdfc9c3e168541572163bb7a
4m1nr <amin.rezaiemehr@gmail.com>
2024-06-24 16:44:05 +0330

    some changes in user constructor and setters

--- src/User.java
+++ src/User.java
@@ -4,35 +4,46 @@ import java.util.List;
 
 public class User {
     private final static ArrayList<String> roles = (ArrayList<String>) List.of("admin","user");
-    private final String id;
+    private /*should be final ?*/ String id;
     private String name, lastName, phoneNumber, emailAddress;
     private String hashedPassword,role;
     private ArrayList<Address> adrresses;
 
     public User(String id, String name, String lastName, String phoneNumber, String emailAddress, String hashedPassword, ArrayList<Address> addresses, String role) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.emailAddress = emailAddress;
+        setId(id);
+        setName(name);
+        setLastName(lastName);
+        setPhoneNumber(phoneNumber);
+        setEmailAddress(emailAddress);
         this.hashedPassword = hashedPassword;
         this.adrresses = addresses;
-        this.role = role;
+        setRole(role);
     }
 
     public String getId() {return id;}
+    private void setId(String id) {
+        //TODO
+        this.id = id;}
 
     public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    public void setName(String name) {
+        //TODO
+        this.name = name;}
 
     public String getLastName() {return lastName;}
-    public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setLastName(String lastName) {
+        //TODO
+        this.lastName = lastName;}
 
     public String getPhoneNumber() {return phoneNumber;}
-    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
+    public void setPhoneNumber(String phoneNumber) {
+        //TODO
+        this.phoneNumber = phoneNumber;}
 
     public String getEmailAddress() {return emailAddress;}
-    public void setEmailAddress(String emailAddress) {this.emailAddress = emailAddress;}
+    public void setEmailAddress(String emailAddress) {
+        //TODO
+        this.emailAddress = emailAddress;}
 
     public String getRole() {return role;}
     public void setRole(String role) {if (roles.contains(role)) this.role = role;}

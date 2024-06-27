9b8d7108eac3b50547680fdddfd37b6294ebf527
Hooman <hooman.hamidi.poor@gmail.com>
2024-06-27 15:50:19 +0330

    Added Serialize feature

--- src/Cart.java
+++ src/Cart.java
@@ -2,18 +2,35 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Cart {
-    private ArrayList<Product> products = new ArrayList<    >();
+    private ArrayList<String> products = new ArrayList<>();
+
+    public Cart(){}
+    public Cart(String serializedCart) {
+        String[] products = serializedCart.split("-");
+        for (int i = 0; i < products.length; i++) this.addProduct(products[i]);
+    }
 
     //getter
-    public ArrayList<Product> getCart() {return products;}
+    public ArrayList<String> getCart() {return products;}
 
     //setters
     public void addProduct(Product product) {
-        products.add(product);}
+        products.add(product.getId());}
+    public void addProduct(String ID){
+        products.add(ID);}
     public void removeProduct(Product product) {
-        products.remove(product);}
-    public void removeProduct(int index) {
-        products.remove(index);}
+        products.remove(product.getId());}
+    public void removeProduct(String ID) {
+        products.remove(ID);}
     public void removeAllProducts() {
         products.clear();}
+
+    public String getSerializedCart(){
+        if (products.isEmpty()) return "";
+        String string = products.get(0);
+        for (int i = 1; i < products.size(); i++) string += "-" + products.get(i);
+        return string;
+    }
+
+
 }

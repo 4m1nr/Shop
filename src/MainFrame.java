d66bfc6e081a0a213ace62a209661257accd9a62
Mehran Mohammadi <161492709+mehrun24@users.noreply.github.com>
2024-06-28 15:49:46 +0330

    Added main frame (header + products)

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,6 +5,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import javax.swing.SpringLayout;
+import net.miginfocom.swing.MigLayout;
+import java.awt.Color;
 
 public class MainFrame extends JFrame {
 
@@ -32,24 +37,35 @@ public class MainFrame extends JFrame {
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 586, 407);
+		setBounds(100, 100, 867, 528);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{70, 0, 0};
+		gbl_contentPane.columnWidths = new int[]{562, 0};
+		gbl_contentPane.rowHeights = new int[]{72, 400, 0};
 		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
+		JPanel panel_1 = new JPanel();
+		//To do: panel_1 is Header. try it mr Amin and begu shod ya na!
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		contentPane.add(panel_1, gbc_panel_1);
+		
 		JPanel panel = new JPanel();
+		//To do: panel is products panel.
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.fill = GridBagConstraints.BOTH;
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 1;
 		contentPane.add(panel, gbc_panel);
+		panel.setLayout(new BorderLayout(0, 0));
 	}
 
 }

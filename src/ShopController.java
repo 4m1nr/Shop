ea98c34ecebe2dac8c83812310ab3881b26837cf
4m1nr <amin.rezaiemehr@gmail.com>
2024-06-28 11:48:41 +0330

    added ShopController basic

---
+++ src/ShopController.java
@@ -0,0 +1,34 @@
+public interface ShopController {
+    public User login(String phoneNumber, String password);
+    public void logout();
+    public User register(String phoneNumber, String passwordFirst,String passwordSecond);
+
+    public void addProduct(String name, double price, int stock, String category, String description);
+    public void removeProduct(String productID);
+    public void updateProduct(String productID, String name, double price, int stock, String category, String description);
+
+    public void viewCart();
+    public void addProductToCart(String productID, int quantity);
+    public void removeProductFromCart(String productID);
+    public void updateProductInCart(String productID, int quantity);
+    public void checkout();
+
+    public void viewOrders();
+    public void viewOrderDetails(String orderID);
+
+    public void viewProducts();
+    public void viewProductDetails(String productID);
+
+    public void viewProfile();
+    public void updateProfile(String userID, String password);
+
+    public void viewUsers();
+    public void viewUserDetails(String ID);
+    public void changeUserRole(String ID, String role);
+    public void removeUser(String username);
+
+    public void viewUserOrders(String userId);
+    public void viewUserOrderDetails(String userID, String orderID);
+    public void viewUserCart(String username);
+    public void viewUserProfile(String userId);
+}

7568a7fb2537a5d42b8c0a9b6504f416a0fe07f2
4m1nr <amin.rezaiemehr@gmail.com>
2024-06-28 12:16:54 +0330

    ...

--- src/ProductService.java
+++ src/ProductService.java
@@ -1,2 +1,13 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface ProductService {
+    public void insertProduct(Product product) throws SQLException;
+    public void deleteProduct(Product product) throws SQLException;
+    public void updateProduct(Product product) throws SQLException;
+    public Product getProduct(String id) throws SQLException;
+    public ArrayList<Product> getAllProducts() throws SQLException;
+    public ArrayList<Product> getProductsByCategory(String category) throws SQLException;
+    public ArrayList<Product> getProductsByCategorySorted(String category ,String sortByThis,int page,int numPerPage) throws SQLException;
+    public ArrayList<Product> getProductsSorted(String sortByThis, int page, int numPerPage) throws SQLException;
 }
--- src/UserService.java
+++ src/UserService.java
@@ -1,2 +1,12 @@
-public class UserService {
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface UserService {
+    public void insertUser(User user) throws SQLException;
+    public void deleteUser(User user) throws SQLException;
+    public void updateUser(User user) throws SQLException;
+    public User getUser(String id) throws SQLException;
+    public ArrayList<User> getAllUsers() throws SQLException;
+    public ArrayList<User> getUsersByRole(String role) throws SQLException;
+
 }

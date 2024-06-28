161f36bb377872a01ade27370b6ded99fe625cb3
Mehran Mohammadi <161492709+mehrun24@users.noreply.github.com>
2024-06-29 00:41:56 +0330

    Update ProductPanel.java

--- src/ProductPanel.java
+++ src/ProductPanel.java
@@ -7,6 +7,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Insets;
 
 public class ProductPanel extends JPanel {
 
@@ -28,20 +29,43 @@ public class ProductPanel extends JPanel {
 		Icon imageIcon=new ImageIcon(product.getImages().get(0));
 		
 		setBackground(new Color(128, 255, 255));
-		setLayout(null);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{280, 0};
+		gridBagLayout.rowHeights = new int[]{277, 25, 25, 25, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
-		JLabel lblNewLabel = new JLabel("product icon");
-		lblNewLabel.setBounds(10, 10, 280, 168);
-		lblNewLabel.setIcon(imageIcon); //To do
-		add(lblNewLabel);
+		JLabel iconLabel = new JLabel();
+		iconLabel.setIcon(imageIcon);
+		GridBagConstraints gbc_iconLabel = new GridBagConstraints();
+		gbc_iconLabel.fill = GridBagConstraints.VERTICAL;
+		gbc_iconLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_iconLabel.gridx = 0;
+		gbc_iconLabel.gridy = 0;
+		add(iconLabel, gbc_iconLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel(productName); //To do
-		lblNewLabel_1.setBounds(10, 188, 280, 57); 
-		add(lblNewLabel_1);
+		JLabel nameLabel = new JLabel(productName);
+		GridBagConstraints gbc_nameLabel = new GridBagConstraints();
+		gbc_nameLabel.fill = GridBagConstraints.BOTH;
+		gbc_nameLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_nameLabel.gridx = 0;
+		gbc_nameLabel.gridy = 1;
+		add(nameLabel, gbc_nameLabel);
 		
-		JLabel lblNewLabel_2 = new JLabel(productRate); //To do
-		lblNewLabel_2.setBounds(10, 255, 280, 35);
-		add(lblNewLabel_2);
+		JLabel rateLabel = new JLabel(productRate);
+		GridBagConstraints gbc_rateLabel = new GridBagConstraints();
+		gbc_rateLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_rateLabel.fill = GridBagConstraints.BOTH;
+		gbc_rateLabel.gridx = 0;
+		gbc_rateLabel.gridy = 2;
+		add(rateLabel, gbc_rateLabel);
+		
+		JLabel priceLabel = new JLabel("price???");
+		GridBagConstraints gbc_priceLabel = new GridBagConstraints();
+		gbc_priceLabel.gridx = 0;
+		gbc_priceLabel.gridy = 3;
+		add(priceLabel, gbc_priceLabel);
 
 	}
 }

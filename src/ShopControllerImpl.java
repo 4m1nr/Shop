eecd60da4cadfc246a7e7594eb97628d4390c750
4m1nr <amin.rezaiemehr@gmail.com>
2024-06-28 11:50:58 +0330

    added ShopController basic

---
+++ src/ShopControllerImpl.java
@@ -0,0 +1,126 @@
+public class ShopControllerImpl implements ShopController{
+    @Override
+    public User login(String phoneNumber, String password) {
+        return null;
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public User register(String phoneNumber, String passwordFirst, String passwordSecond) {
+        return null;
+    }
+
+    @Override
+    public void addProduct(String name, double price, int stock, String category, String description) {
+
+    }
+
+    @Override
+    public void removeProduct(String productID) {
+
+    }
+
+    @Override
+    public void updateProduct(String productID, String name, double price, int stock, String category, String description) {
+
+    }
+
+    @Override
+    public void viewCart() {
+
+    }
+
+    @Override
+    public void addProductToCart(String productID, int quantity) {
+
+    }
+
+    @Override
+    public void removeProductFromCart(String productID) {
+
+    }
+
+    @Override
+    public void updateProductInCart(String productID, int quantity) {
+
+    }
+
+    @Override
+    public void checkout() {
+
+    }
+
+    @Override
+    public void viewOrders() {
+
+    }
+
+    @Override
+    public void viewOrderDetails(String orderID) {
+
+    }
+
+    @Override
+    public void viewProducts() {
+
+    }
+
+    @Override
+    public void viewProductDetails(String productID) {
+
+    }
+
+    @Override
+    public void viewProfile() {
+
+    }
+
+    @Override
+    public void updateProfile(String userID, String password) {
+
+    }
+
+    @Override
+    public void viewUsers() {
+
+    }
+
+    @Override
+    public void viewUserDetails(String ID) {
+
+    }
+
+    @Override
+    public void changeUserRole(String ID, String role) {
+
+    }
+
+    @Override
+    public void removeUser(String username) {
+
+    }
+
+    @Override
+    public void viewUserOrders(String userId) {
+
+    }
+
+    @Override
+    public void viewUserOrderDetails(String userID, String orderID) {
+
+    }
+
+    @Override
+    public void viewUserCart(String username) {
+
+    }
+
+    @Override
+    public void viewUserProfile(String userId) {
+
+    }
+}

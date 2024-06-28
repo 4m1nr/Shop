67591a40acffde1832144058eeceb875e09d85d9
Hooman <hooman.hamidi.poor@gmail.com>
2024-06-28 21:33:52 +0330

    Added userID

--- src/Order.java
+++ src/Order.java
@@ -2,23 +2,23 @@ import java.util.ArrayList;
 
 public class Order {
     private ArrayList<String> productsId;
+    private String userID;
 
     public Order(String serializedOrder) {
         productsId = new ArrayList<>();
-        if (serializedOrder != null){
-            String[] products = serializedOrder.split("-");
-            for (String product : products) this.addProduct(product);
-        }
+        this.setUserID(serializedOrder.split(":")[0]);
+        String[] products = serializedOrder.split(":")[1].split("-");
+        for (String product : products) this.addProduct(product);
     }
 
-    public Order(Cart cart) {productsId = (ArrayList<String>) cart.getCart().clone();}
-
-    public Order() {
-        productsId = new ArrayList<>();
+    public Order(Cart cart,String userID) {
+        productsId = (ArrayList<String>) cart.getCart().clone();
+        this.setUserID(userID);
     }
 
     //getter
     public ArrayList<String> getOrder() {return productsId;}
+    public String getUserID() {return userID;}
 
     //setters
     private void addProduct(Product product)    {productsId.add(product.getId());}
@@ -26,6 +26,7 @@ public class Order {
     private void removeProduct(Product product) {productsId.remove(product.getId());}
     private void removeProduct(String ID)       {productsId.remove(ID);}
     private void removeAllProducts()            {productsId.clear();}
+    private void setUserID(String userID)       {this.userID = userID;}
 
     public String getSerializedOrder() {
         if (productsId.isEmpty()) {
@@ -33,7 +34,7 @@ public class Order {
         } else {
             StringBuilder string = new StringBuilder();
             for (String product : productsId) string.append(productsId).append("-");
-            return string.toString();
+            return userID + ":" + string.toString();
         }
     }
 }

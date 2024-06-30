8146ef6bf0b7972f42177d1488f55907ddde4bbe
Mehran Mohammadi <161492709+mehrun24@users.noreply.github.com>
2024-06-30 13:04:10 +0330

    Update AdminSeeOrdersPanel.java

--- src/AdminSeeOrdersPanel.java
+++ src/AdminSeeOrdersPanel.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import javax.swing.JTable;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import java.awt.Component;
 
 public class AdminSeeOrdersPanel extends JPanel {
 
@@ -17,12 +18,12 @@ public class AdminSeeOrdersPanel extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public AdminSeeOrdersPanel(ArrayList<Order> orders) {
+	public AdminSeeOrdersPanel(){//ArrayList<Order> orders) {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{10, 2, 75, 82, 10, 0};
-		gridBagLayout.rowHeights = new int[]{10, 358, 15, 0};
+		gridBagLayout.rowHeights = new int[]{25, 358, 25, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		
@@ -39,6 +40,34 @@ public class AdminSeeOrdersPanel extends JPanel {
 		add(scrollPane_1, gbc_scrollPane_1);
 		JTable table_1 = new JTable(data, columnNames);
 		scrollPane_1.setViewportView(table_1);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 3;
+		gbc_panel.gridy = 2;
+		add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel sumLabel = new JLabel("Sum:");
+		GridBagConstraints gbc_sumLabel = new GridBagConstraints();
+		gbc_sumLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_sumLabel.gridx = 0;
+		gbc_sumLabel.gridy = 0;
+		panel.add(sumLabel, gbc_sumLabel);
+		
+		JLabel sumForRealLabel = new JLabel("**sum**");
+		GridBagConstraints gbc_sumForRealLabel = new GridBagConstraints();
+		gbc_sumForRealLabel.anchor = GridBagConstraints.WEST;
+		gbc_sumForRealLabel.gridx = 1;
+		gbc_sumForRealLabel.gridy = 0;
+		panel.add(sumForRealLabel, gbc_sumForRealLabel);
 
 	}
 

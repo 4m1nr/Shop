cfc3c2811cf843b654e4945e48484304cbf08bed
Mehran Mohammadi <161492709+mehrun24@users.noreply.github.com>
2024-06-30 08:06:11 +0330

    Update ProductInCartPanel.java

--- src/ProductInCartPanel.java
+++ src/ProductInCartPanel.java
@@ -4,10 +4,12 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class ProductInCartPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField countTextField;
 
 	/**
 	 * Create the panel.
@@ -17,9 +19,9 @@ public class ProductInCartPanel extends JPanel {
 	public ProductInCartPanel() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{192, 15, 15, 8, 0};
+		gridBagLayout.rowHeights = new int[]{192, 24, 24, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel productImage = new JLabel("Product icon");
@@ -37,30 +39,38 @@ public class ProductInCartPanel extends JPanel {
 		gbc_panel.gridy = 1;
 		add(panel, gbc_panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0};
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0};
 		gbl_panel.rowHeights = new int[]{0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
 		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Count:");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 0;
-		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		JLabel countLabel = new JLabel("**count**");
+		JLabel countLabel = new JLabel("Count:");
 		GridBagConstraints gbc_countLabel = new GridBagConstraints();
-		gbc_countLabel.anchor = GridBagConstraints.WEST;
-		gbc_countLabel.gridx = 1;
+		gbc_countLabel.anchor = GridBagConstraints.EAST;
+		gbc_countLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_countLabel.gridx = 0;
 		gbc_countLabel.gridy = 0;
 		panel.add(countLabel, gbc_countLabel);
 		
+		countTextField = new JTextField();
+		GridBagConstraints gbc_countTextField = new GridBagConstraints();
+		gbc_countTextField.anchor = GridBagConstraints.WEST;
+		gbc_countTextField.insets = new Insets(0, 0, 0, 5);
+		gbc_countTextField.gridx = 1;
+		gbc_countTextField.gridy = 0;
+		panel.add(countTextField, gbc_countTextField);
+		countTextField.setColumns(10);
+		
+		JButton saveButton = new JButton("Save");
+		GridBagConstraints gbc_saveButton = new GridBagConstraints();
+		gbc_saveButton.anchor = GridBagConstraints.WEST;
+		gbc_saveButton.gridx = 2;
+		gbc_saveButton.gridy = 0;
+		panel.add(saveButton, gbc_saveButton);
+		
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
 		gbc_panel_1.gridx = 0;
 		gbc_panel_1.gridy = 2;
@@ -86,13 +96,6 @@ public class ProductInCartPanel extends JPanel {
 		gbc_finalPriceLabel.gridx = 1;
 		gbc_finalPriceLabel.gridy = 0;
 		panel_1.add(finalPriceLabel, gbc_finalPriceLabel);
-		
-		JButton buyButton = new JButton("Buy");
-		GridBagConstraints gbc_buyButton = new GridBagConstraints();
-		gbc_buyButton.anchor = GridBagConstraints.NORTH;
-		gbc_buyButton.gridx = 0;
-		gbc_buyButton.gridy = 3;
-		add(buyButton, gbc_buyButton);
 
 	}
 

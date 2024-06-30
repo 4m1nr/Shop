6f0f8e696b3025e5770ecd9c3594fa33e810c5f8
Mehran Mohammadi <161492709+mehrun24@users.noreply.github.com>
2024-06-30 10:02:30 +0330

    Add files via upload

---
+++ src/AdminSeeCustomers.java
@@ -0,0 +1,43 @@
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
+public class AdminSeeCustomers extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTable table;
+
+	/**
+	 * Create the panel.
+	 */
+	public AdminSeeCustomers() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{10, 2, 75, 82, 10, 0};
+		gridBagLayout.rowHeights = new int[]{10, 358, 15, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		
+		String[] columnNames = {"Customer ID", "Name", "Last name", "Phone number" , "Total verified order" };
+		String data[][] = new String[200][5];
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.gridwidth = 3;
+		gbc_scrollPane_1.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 1;
+		gbc_scrollPane_1.gridy = 1;
+		add(scrollPane_1, gbc_scrollPane_1);
+		JTable table_1 = new JTable(data, columnNames);
+		scrollPane_1.setViewportView(table_1);
+
+	}
+
+}
